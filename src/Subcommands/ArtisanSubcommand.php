abe9aba4328d9661c9b22561627a6b2fa49a60b5
Nasyrov Evgenii <inasyrov@ya.ru>
2020-06-21 17:26:55 +0100

    Add artisan subcommand

---
+++ src/Subcommands/ArtisanSubcommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Nasyrov\Laravel\LocalServer\Subcommands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ArtisanSubcommand extends Subcommand
+{
+    const COMMAND = 'docker-compose exec -T -u nobody artisan %s';
+
+    public function __invoke(InputInterface $input, OutputInterface $output): int
+    {
+        $options = $input->getArgument('options');
+        $options = implode(' ', $options);
+
+        return $this->runProcess(static::COMMAND, $options);
+    }
+}
