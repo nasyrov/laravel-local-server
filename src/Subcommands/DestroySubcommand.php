3a7bbcc174169c6242c8deb1861ca470447f1586
Nasyrov Evgenii <inasyrov@ya.ru>
2020-06-21 17:20:18 +0100

    Add destroy subcommand

---
+++ src/Subcommands/DestroySubcommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nasyrov\Laravel\LocalServer\Subcommands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DestroySubcommand extends Subcommand
+{
+    const COMMAND = 'docker-compose down -v';
+
+    public function __invoke(InputInterface $input, OutputInterface $output): int
+    {
+        return $this->runProcess(static::COMMAND);
+    }
+}
